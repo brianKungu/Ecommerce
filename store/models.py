2687f77ee018b3f68dd3fc83bdb4e139809c3ae8
brianKungu <bkungu07@gmail.com>
2020-11-15 17:15:16 +0300

    Ecommerce-pyton

---
+++ store/models.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+
+#items that can be purchased
+class Item(models.Model):
+    
+    title=models.CharField(max_length=100)
+    price=models.FloatField()
+    
+    def __str__(self):
+        return self.title
+    
+
+#items in the cart
+class OrderItem(models.Model):
+    item=models.ForeignKey(Item,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+#cart
+class Order(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    items=models.ManyToManyField(OrderItem)
+    start_date=models.DateTimeField(auto_now_add=True)
+    order_date=models.DateTimeField()
+    ordered=models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.username
---
+++ store/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Item
+# Create your views here.
+
+def store(request):
+    context={
+        'items':Item.objects.all()
+    }
+    return render(request,'store/store.html',context)
+
+def cart(request):
+    context={}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    context={}
+    return render(request, 'store/checkout.html', context)
